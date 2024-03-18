b7616555d3b5b9aee236d4996c05b9e02d253739
Jay <zjh6252007@gmail.com>
2024-03-17 20:25:39 -0400

    initial commit

---
+++ src/main/java/com/jay/pmanage/PmanageApplication.java
@@ -0,0 +1,13 @@
+package com.jay.pmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jay/pmanage/PmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.pmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
