a84e7d300bbaf92beb1ef753fe47e8368e9cf246
Jay <zjh6252007@gmail.com>
2024-04-05 20:59:21 -0400

    create Properties pojo

---
+++ src/main/java/com/jay/pmanage/pojo/Properties.java
@@ -0,0 +1,15 @@
+package com.jay.pmanage.pojo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Properties {
+    private Integer id;
+    private String address;
+    private String state;
+    private BigDecimal price;
+
+
+}
